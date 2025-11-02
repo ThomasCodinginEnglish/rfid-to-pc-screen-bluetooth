fba300a4346ed63b7d77b0812442cd6b500deb72
Thomas Coding <thomascodinginenglish@gmail.com>
2025-11-02 14:51:30 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,28 @@
 let textSprite: TextSprite = null
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    textSprite = textsprite.create("Hello, Kanna.")
-    textSprite.setMaxFontHeight(10)
-    textSprite.setPosition(80, 42)
+    textSprite = textsprite.create("Hello, Kanna.", 0, 3)
+    movement()
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    textSprite = textsprite.create("Hello, Leon.")
-    textSprite.setMaxFontHeight(10)
-    textSprite.setPosition(80, 42)
+    textSprite = textsprite.create("Hello, Leon.", 0, 2)
+    movement()
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    textSprite = textsprite.create("Hello, Juki.")
+function movement () {
+    music.play(music.createSong(hex`0078000408020200001c00010a006400f401640000040000000000000000000000000005000004240000000400011e04000800012210001400012518001c0001221c002000012220002400011e07001c00020a006400f4016400000400000000000000000000000000000000031e0008000c0001220c00100001251400180001221c002000011e20002400011e`), music.PlaybackMode.InBackground)
     textSprite.setMaxFontHeight(10)
-    textSprite.setPosition(80, 42)
+    textSprite.setPosition(80, 120)
+    textSprite.setBounceOnWall(true)
+    textSprite.setVelocity(0, randint(-30, -50))
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    textSprite = textsprite.create("Hello, Juki.", 0, 8)
+    movement()
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    textSprite = textsprite.create("Hello, Kiko.")
-    textSprite.setMaxFontHeight(10)
-    textSprite.setPosition(80, 42)
+    textSprite = textsprite.create("Hello, Kiko.", 0, 7)
+    movement()
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    textSprite = textsprite.create("Hello, Runa.")
-    textSprite.setMaxFontHeight(10)
-    textSprite.setPosition(80, 42)
+    textSprite = textsprite.create("Hello, Runa.", 0, 5)
+    movement()
 })
